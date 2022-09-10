6863c00f4ce8797034da11757e4c3813e119679f
Dennis Demir <demir.petrus@gmail.com>
2022-09-10 09:59:41 +0200

    Finished project

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -96,6 +96,8 @@ const Profile = () => {
     }
   };
 
+  const onEdit =  (listingId) => navigate(`/edit-listing/${listingId}`)
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -154,6 +156,7 @@ const Profile = () => {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
